bbcd4ad1623d5e6ea504dd65541efa7ab9f910de
Skaara9111 <skaara911@gmail.com>
2022-04-07 11:17:02 +0300

    change about fragment

--- app/src/main/java/com/example/foodinformation/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/foodinformation/ui/about/AboutFragment.kt
@@ -1,60 +1,68 @@
 package com.example.foodinformation.ui.about
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodinformation.databinding.FragmentAboutBinding
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.foodinformation.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AboutFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AboutFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val viewModel: AboutViewModel by lazy {
+        ViewModelProvider(this).get(AboutViewModel::class.java)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val binding = FragmentAboutBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        binding.text2.setOnClickListener {
+            showAlertDialog()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AboutFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AboutFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun showAlertDialog() {
+        val alertDialog: AlertDialog? = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setPositiveButton("ОК",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        openUrl()
+                    })
+                setNegativeButton("Отмена",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        dialog.cancel()
+                    })
             }
+            builder.create()
+        }
+        alertDialog?.setMessage("Открыть сайт в браузере?")
+        alertDialog?.create()
+        alertDialog?.show()
+    }
+
+
+    private fun openUrl() {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+        startActivity(browserIntent)
     }
 }
---
+++ app/src/main/java/com/example/foodinformation/ui/about/AboutViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.foodinformation.ui.about
+
+import android.app.AlertDialog
+import android.app.Application
+import android.content.DialogInterface
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.withContext
+
+class AboutViewModel : ViewModel() {
+
+
+
+}
