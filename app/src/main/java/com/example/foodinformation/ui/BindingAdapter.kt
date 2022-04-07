caaf46e6effa1840a42c0a38b5bf79ea15fc68c7
Skaara9111 <skaara911@gmail.com>
2022-04-07 11:17:57 +0300

    add bing Image with glide

--- app/src/main/java/com/example/foodinformation/ui/BindingAdapter.kt
+++ app/src/main/java/com/example/foodinformation/ui/BindingAdapter.kt
@@ -1,12 +1,29 @@
 package com.example.foodinformation.ui
 
+
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.foodinformation.data.model.Food
 import com.example.foodinformation.ui.overview.FoodListAdapter
+
 
 @BindingAdapter("listData")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<Food>?) {
     val adapter = recyclerView.adapter as FoodListAdapter
     adapter.submitList(data)
 }
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    val context = imgView.context
+    val drawableResourceId: Int =
+        context.resources.getIdentifier(imgUrl, "drawable", context.packageName)
+    imgUrl?.let {
+        Glide.with(imgView.context)
+            .load(drawableResourceId)
+            .into(imgView)
+    }
+}
+
